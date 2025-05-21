2aa40065135a8b81bc670693d94773f88ba0ead3
waimea-wmbutler <168692054+waimea-wmbutler@users.noreply.github.com>
2025-05-22 11:33:24 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 2025-200dtd-flask-into
+Setting up a Flask web app for the first time
---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from random import randint
+
+# Create the app
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    return render_template('pages/home.jinja')
+
+@app.get("/about/")
+def about():
+    return render_template('pages/about.jinja')
+
+@app.get("/random/")
+def random():
+    randNum = randint(1, 1000000000000000)
+    return render_template('pages/random.jinja', number=randNum)
